07b445b8f5293104e7e0236573df67bd5aaa300d
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-04 21:19:28 +0700

    feat(colorizer): enable all
    + .config/nvim/after/plugin/colorizer.rc.lua
    + Change colorizer setup to enable all costom highlight.
    + Effect easy to distinguish.

--- .config/nvim/after/plugin/colorizer.rc.lua
+++ .config/nvim/after/plugin/colorizer.rc.lua
@@ -3,4 +3,15 @@ if (not status) then return end
 
 colorizer.setup({
   '*';
+}, {
+  RGB      = true; -- #RGB hex codes
+  RRGGBB   = true; -- #RRGGBB hex codes
+  names    = true; -- "Name" codes like Blue
+  RRGGBBAA = true; -- #RRGGBBAA hex codes #8a4af3;
+  rgb_fn   = true; -- CSS rgb() and rgba() functions
+  hsl_fn   = true; -- CSS hsl() and hsla() functions
+  css      = true; -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  css_fn   = true; -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  -- Available modes: foreground, background
+  mode     = 'background'; -- Set the display mode.
 })
