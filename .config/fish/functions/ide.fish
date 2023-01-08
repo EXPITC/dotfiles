8381996eb9274592df4ea8aab96e599a420e4184
ᴹᵃᵈᵉ ᵒⁿ ᴱᵃʳᵗʰ ᵇʸ ʰᵘᵐᵃⁿˢ <39050604+EXPITC@users.noreply.github.com>
2023-01-08 17:57:56 +0700

    feat(fish): adding new ide 3 panel
    + .config/fish/functions/ide.fish
    + Adding new binding `ide` for 3 panel.
    + I think it more comfortable to use 1 panel to edit, one to read
      running condition and the last one for git uses; for backend i can use
      another tmux tab for more organize I think.

--- .config/fish/functions/ide.fish
+++ .config/fish/functions/ide.fish
@@ -1,3 +1,8 @@
+ # Version 4 panel
+# function ide --wraps='tmux split-window -v -p 30 && tmux split-window -h -p 66 && tmux split-window -h -p 50 || tmux' --description 'alias ide tmux split-window -v -p 30 && tmux split-window -h -p 66 && tmux split-window -h -p 50 || tmux'
+#   tmux split-window -v -p 30 && tmux split-window -h -p 66 && tmux split-window -h -p 50 || tmux $argv; 
+# end
+# Version 3 panel
 function ide --wraps='tmux split-window -v -p 30 && tmux split-window -h -p 66 && tmux split-window -h -p 50 || tmux' --description 'alias ide tmux split-window -v -p 30 && tmux split-window -h -p 66 && tmux split-window -h -p 50 || tmux'
-  tmux split-window -v -p 30 && tmux split-window -h -p 66 && tmux split-window -h -p 50 || tmux $argv; 
+  tmux split-window -v -p 30 && tmux split-window -h -p 40 || tmux $argv; 
 end
